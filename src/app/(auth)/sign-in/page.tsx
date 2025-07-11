929907b540672a51e215a8eb2e32d2943aa7937f
Yusuf <yusufamin876@gmail.com>
2025-07-12 01:11:09 +0530

    04. Authentication-UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { Card } from "@/components/ui/card";
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view";
+
+const Page = () => {
+    return <SignInView />
+}
+
+export default Page;
