896d12d1ee6fed72cf9d551d6b5d249d5dda1f60
Yusuf <yusufamin876@gmail.com>
2025-07-17 14:53:24 +0530

    09. Agents-Setup

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,5 @@
+import { nanoid } from "nanoid";
+
 import { pgTable, text, timestamp, boolean } from "drizzle-orm/pg-core";
 
 
@@ -45,4 +47,17 @@ export const verification = pgTable("verification", {
  expiresAt: timestamp('expires_at').notNull(),
  createdAt: timestamp('created_at').$defaultFn(() => /* @__PURE__ */ new Date()),
  updatedAt: timestamp('updated_at').$defaultFn(() => /* @__PURE__ */ new Date())
+});
+
+export const agents = pgTable("agents", {
+    id: text("id")
+        .primaryKey()
+        .$defaultFn(() => nanoid()),
+    name: text("name").notNull(),
+    userId: text("user.id")
+        .notNull()
+        .references(() => user.id, { onDelete: "cascade" }),
+    instructions: text("instructions").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
