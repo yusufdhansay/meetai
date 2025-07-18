91c0f23157cad4a98ef3d6937441399da13bc61f
Yusuf <yusufamin876@gmail.com>
2025-07-18 15:14:25 +0530

    13. Agents-Filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -6,6 +6,8 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
+import type { SearchParams } from "nuqs";
+import { loadSearchParams } from "@/modules/agents/params";
 import { redirect } from "next/navigation";
 
 import { 
@@ -14,7 +16,13 @@ import {
     AgentsViewLoading 
 } from "@/modules/agents/ui/views/agents-view";
 
-const Page = async () => {
+interface Props {
+    searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+    const filters = await loadSearchParams(searchParams);
+    
     const session = await auth.api.getSession({
         headers: await headers(),
     });
@@ -25,7 +33,9 @@ const Page = async () => {
 
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <>
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -9,15 +9,26 @@ import { ErrorState } from "@/components/error-state";
 import { DataTable } from "../components/data-table";
 import { columns } from "../components/columns";
 import { EmptyState } from "@/components/empty-state";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { DataPagination } from "../components/data-pagination";
 
 export const AgentsView = () => {
+    const [filters, setFilters] = useAgentsFilters();
+
     const trpc = useTRPC();
-    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+    const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+        ...filters,
+    }));
 
     return (
         <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <DataTable data={data} columns={columns} />
-            {data.length === 0 && (
+            <DataTable data={data.items} columns={columns} />
+            <DataPagination 
+                page={filters.page}
+                totalPages={data.totalPages}
+                onPageChange={(page) => setFilters({ page })}
+            />
+            {data.items.length === 0 && (
                 <EmptyState
                     title="Create Your Personalized AI Agent"
                     description="Design a custom AI agent tailored to your needs. Each agent 
