1a3248cf3a3d0ec52a48c1a72e922e1227ee82dc
Yusuf <yusufamin876@gmail.com>
2025-07-19 17:58:57 +0530

    16. Meetings-Setup

---
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const MeetingsView = () => {
+    const trpc = useTRPC();
+    const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
+
+    return (
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    );
+};
+
+export const MeetingsViewLoading = () => {
+    return (
+        <LoadingState 
+            title="Loading Meetings"
+            description="This may take a few seconds"
+        />
+    );
+};
+
+export const MeetingsViewError = () => {
+    return (
+        <ErrorState 
+            title="Error Loading Meetings"
+            description="Something went wrong"
+        />
+    );
+};
