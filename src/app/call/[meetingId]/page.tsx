8a27443b1a08846ca524aa3be88a0faedbeb8897
Yusuf <yusufamin876@gmail.com>
2025-07-25 17:32:23 +0530

    22. Video-Call

---
+++ src/app/call/[meetingId]/page.tsx
@@ -0,0 +1,37 @@
+import { auth } from "@/lib/auth";
+import { CallView } from "@/modules/call/ui/views/call-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface Props {
+    params: Promise<{
+        meetingId: string;
+    }>;
+};
+
+const Page = async ({ params }: Props) => {
+    const session = await auth.api.getSession({
+        headers: await headers(),
+    });
+
+    if (!session) {
+        redirect("/sign-in");
+    }
+
+    const { meetingId } = await params;
+
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.meetings.getOne.queryOptions({ id: meetingId }),
+    );
+
+    return (
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <CallView meetingId={meetingId} />
+        </HydrationBoundary>
+    );
+};
+
+export default Page;
