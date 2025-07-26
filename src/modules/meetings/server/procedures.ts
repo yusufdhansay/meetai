07464893ef7a180b7b52b0e7d46620093f637b7c
Yusuf <yusufamin876@gmail.com>
2025-07-26 20:30:53 +0530

    26. Transcript-Chat

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -1,17 +1,118 @@
 import { db } from "@/db";
-import { agents, meetings } from "@/db/schema";
+import { agents, meetings, user } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 
 import { z } from "zod";
-import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
+import JSONL from "jsonl-parse-stringify";
+import { and, count, desc, eq, getTableColumns, ilike, inArray, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
-import { MeetingStatus } from "../types";
+import { MeetingStatus, StreamTranscriptItems } from "../types";
 import { streamVideo } from "@/lib/stream-video";
 import { generateAvatarUri } from "@/lib/avatar";
+import { streamChat } from "@/lib/stream-chat";
 
 export const meetingsRouter = createTRPCRouter({
+    generateChatToken: protectedProcedure.mutation(async ({ ctx }) => {
+        const token = streamChat.createToken(ctx.auth.user.id);
+        await streamChat.upsertUser({
+            id: ctx.auth.user.id,
+            role: "admin",
+        });
+
+        return token;
+    }),
+    getTranscript: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input, ctx }) => {
+            const [existingMeeting] = await db
+                .select()
+                .from(meetings)
+                .where(
+                    and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+                );
+
+            if (!existingMeeting) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Meeting Not Found",
+                });
+            }
+
+            if (!existingMeeting.transcriptUrl) {
+                return [];
+            }
+
+            const transcript = await fetch(existingMeeting.transcriptUrl)
+                .then((res) => res.text())
+                .then((text) => JSONL.parse<StreamTranscriptItems>(text))
+                .catch(() => {
+                    return [];
+                });
+
+            const speakerIds = [
+                ...new Set(transcript.map((item) => item.speaker_id)),
+            ];
+
+            const userSpeakers = await db
+                .select()
+                .from(user)
+                .where(inArray(user.id, speakerIds))
+                .then((users) =>
+                    users.map((user) => ({
+                        ...user,
+                        image:
+                            user.image ??
+                            generateAvatarUri({ seed: user.name, variant: "initials"}),
+                    }))
+                );
+
+            const agentSpeakers = await db
+                .select()
+                .from(agents)
+                .where(inArray(agents.id, speakerIds))
+                .then((agents) =>
+                    agents.map((agent) => ({
+                        ...agent,
+                        image:generateAvatarUri({ 
+                            seed: agent.name, 
+                            variant: "botttsNeutral"
+                        }),
+                    }))
+                );
+
+            const speakers = [...userSpeakers, ...agentSpeakers];
+
+            const transcriptWithSpeakers = transcript.map((item) => {
+                const speaker = speakers.find(
+                    (speaker) => speaker.id === item.speaker_id
+                );
+
+                if (!speaker) {
+                    return {
+                        ...item,
+                        user: {
+                            name: "Unknown",
+                            image: generateAvatarUri({
+                                seed: "Unknown",
+                                variant: "initials",
+                            }),
+                        },
+                    };
+                }
+
+                return {
+                    ...item,
+                    user: {
+                        name: speaker.name,
+                        image: speaker.image,
+                    },
+                };
+            })
+
+            return transcriptWithSpeakers;
+        }),
     generateToken: protectedProcedure.mutation(async ({ ctx }) => {
         await streamVideo.upsertUsers([
             {
--- src/modules/meetings/ui/views/meeting-id-view.tsx
+++ src/modules/meetings/ui/views/meeting-id-view.tsx
@@ -14,7 +14,7 @@ import { UpcomingState } from "../components/upcoming-state";
 import { ActiveState } from "../components/active-state";
 import { CancelledState } from "../components/cancelled-state";
 import { ProcessingState } from "../components/processing-state";
-import { CompletedState } from "@/components/completed-state";
+import { CompletedState } from "../components/completed-state";
 
 interface Props {
     meetingId: string;
