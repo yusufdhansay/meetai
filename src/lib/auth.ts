916ab0d81033b0e52d7e6aeb4031d4145408c531
Yusuf <yusufamin876@gmail.com>
2025-07-27 22:44:10 +0530

    29. Quick-Fixes

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,9 +22,9 @@ export const auth = betterAuth({
         }),
     ],
     socialProviders: {
-        github: { 
-            clientId: process.env.GITHUB_CLIENT_ID as string, 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        discord: { 
+            clientId: process.env.DISCORD_CLIENT_ID as string, 
+            clientSecret: process.env.DISCORD_CLIENT_SECRET as string, 
         },
         google: { 
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
